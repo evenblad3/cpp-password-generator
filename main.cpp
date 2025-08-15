b4f43d0e1072e1bec6b66dd31fdfbbd35ee5e74c
Chris Bradley <general.defacing593@passinbox.com>
2025-08-15 17:08:51 +0530

    Update main.cpp
    Now with better randomness ensuring more secure passwords.

--- main.cpp
+++ main.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
-#include <ctime>
+#include <random>
 #include <string>
 #include <vector>
 
 int main()
 {
-	srand(time(0));
 	std::vector<char> password;
 
 
@@ -27,40 +26,47 @@ int main()
 	char lowercaseLetters[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
 	char specialChars[29] = {'!', '#', '$', '%', '&', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '{', '|', '}', '~'};
 
+	std::random_device rd;
+	std::mt19937 mtgen(rd());
+	std::uniform_int_distribution<int> upperRandom(0, (sizeof(uppercaseLetters)/sizeof(uppercaseLetters[0]) - 1));
+	std::uniform_int_distribution<int> lowerRandom(0, (sizeof(lowercaseLetters)/sizeof(lowercaseLetters[0]) - 1));
+	std::uniform_int_distribution<int> specialRandom(0, (sizeof(specialChars)/sizeof(specialChars[0]) - 1));
+	std::uniform_int_distribution<int> numberRandom(0, 9);
+
 	std::cout << "How many characters would you like your password to be?" << std::endl;
 	std::cin >> passwordLength;
 
 	std::cout << "Do you want uppercase letters? (yes/no)" << std::endl;
 	std::cin >> upperChoice;
-	while (upperChoice != "yes" && upperChoice != "no") {
+	while (upperChoice.at(0) != 'y' && upperChoice.at(0) != 'n') {
     	std::cout << "Please enter 'yes' or 'no'!" << std::endl;
     	std::cin >> upperChoice;
 	}
-	includeUppercase = (upperChoice == "yes");
+	includeUppercase = (upperChoice.at(0) == 'y');
 
 	std::cout << "Do you want lowercase letters? (yes/no)" << std::endl;
 	std::cin >> lowerChoice;
-	while (lowerChoice != "yes" && lowerChoice != "no") {
+	while (lowerChoice.at(0) != 'y' && lowerChoice.at(0) != 'n') {
     	std::cout << "Please enter 'yes' or 'no'!" << std::endl;
     	std::cin >> lowerChoice;
 	}
-	includeLowercase = (lowerChoice == "yes");
+	includeLowercase = (lowerChoice.at(0) == 'y');
 
 	std::cout << "Do you want special characters? (yes/no)" << std::endl;
 	std::cin >> specialCharChoice;
-	while (specialCharChoice != "yes" && specialCharChoice != "no") {
+	while (specialCharChoice.at(0) != 'y' && specialCharChoice.at(0) != 'n') {
     	std::cout << "Please enter 'yes' or 'no'!" << std::endl;
     	std::cin >> specialCharChoice;
 	}
-	includeSpecialChars = (specialCharChoice == "yes");
+	includeSpecialChars = (specialCharChoice.at(0) == 'y');
 
 	std::cout << "Do you want numbers? (yes/no)" << std::endl;
 	std::cin >> numberChoice;
-	while (numberChoice != "yes" && numberChoice != "no") {
+	while (numberChoice.at(0) != 'y' && numberChoice.at(0) != 'n') {
     	std::cout << "Please enter 'yes' or 'no'!" << std::endl;
     	std::cin >> numberChoice;
 	}
-	includeNumbers = (numberChoice == "yes");
+	includeNumbers = (numberChoice.at(0) == 'y');
 
 	if (!includeUppercase && !includeLowercase && !includeSpecialChars && !includeNumbers) {
     	std::cout << "Please choose at least one option. Try again!" << std::endl;
@@ -69,22 +75,23 @@ int main()
 
 	std::cout << "Your password is: " << std::endl;
 	while (password.size() < passwordLength) {
-    	int selector = rand() % 4 + 1;
+    	std::uniform_int_distribution<int> _selector(1,4);
+    	int selector = _selector(mtgen);
 
     	if (selector == 1 && includeUppercase) {
-        	int j = rand() % 26;
+        	int j = upperRandom(mtgen);
         	std::cout << uppercaseLetters[j];
         	password.push_back(uppercaseLetters[j]);
     	} else if (selector == 2 && includeLowercase) {
-        	int j = rand() % 26;
+        	int j = lowerRandom(mtgen);
         	std::cout << lowercaseLetters[j];
         	password.push_back(lowercaseLetters[j]);
     	} else if (selector == 3 && includeSpecialChars) {
-        	int j = rand() % 29;
+        	int j = specialRandom(mtgen);
         	std::cout << specialChars[j];
         	password.push_back(specialChars[j]);
     	} else if (selector == 4 && includeNumbers) {
-        	int number = rand() % 10;
+        	int number = numberRandom(mtgen);
         	std::cout << char(number + '0');
         	password.push_back(char(number + '0'));
     	}
